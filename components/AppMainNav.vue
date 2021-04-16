db51b1acaee9c26121fdbfb2f99785c7dc083573
longplayer <bartaber@hotmail.com>
2021-04-16 13:07:13 +0200

    Added new PAGE layout

---
+++ components/AppMainNav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-nav">
+    <logo />
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/news">News</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/foundation">The Foundation</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/artist">The Artist</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/early">Early Works</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/surrealism">Surrealism</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/graphic">Graphic Art</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/photorealism">Photorealism</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/sculpture">Sculptures</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/poetic">Poetic Art</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/latest">Latest Aquisitions</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/biography">Biography</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/bibliography">Bibliography</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/contact">Contact</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <logo :color="'blue'" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,36 @@
 <template>
-  <img class="app-logo" src="~/assets/img/logo-white.jpg" />
+  <!-- <img class="app-logo" :src="require('../assets/img/logo-white.jpg')" /> -->
+  <img class="app-logo" :src="coloredLogo" />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      defaultColor: "white",
+      altColor: "blue"
+    };
+  },
+
+  props: {
+    color: {
+      type: String,
+      required: false,
+      default: "white"
+    }
+  },
+
+  computed: {
+    coloredLogo: function() {
+      // there's only 2 colors
+      let color =
+        this.defaultColor === this.color ? this.defaultColor : this.altColor;
+
+      return require(`@/assets/img/logo-${color}.jpg`);
+    }
+  }
+};
+</script>
 
 <style>
 .app-logo {
