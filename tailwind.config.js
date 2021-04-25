862f83b1483ff7a1eb057403c89a9f9cb9e0d69e
longplayer <bartaber@hotmail.com>
2021-04-25 12:05:13 +0200

    Changed Tailwind global GRAY

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          "100": "#F8F8F8",
+          "200": "#E0E0E0",
+          "300": "#C8C8C8",
+          "400": "#888888",
+          "500": "#707070",
+          "600": "#505050",
+          "700": "#383838",
+          "800": "#282828",
+          "900": "#101010"
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
