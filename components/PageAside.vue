1912045440c6063a4bd1ee007f291e7eabceda35
longplayer <bartaber@hotmail.com>
2021-04-25 12:04:59 +0200

    Added aside component to Bio

---
+++ components/PageAside.vue
@@ -0,0 +1,68 @@
+<template>
+  <aside class="aside--content">
+    <!-- intro -->
+    <!-- main -->
+    <ul class="aside--list">
+      <li
+        class="aside--list-item"
+        v-for="(item, index) in dataSource"
+        :key="index"
+      >
+        <figure class="aside--item-fig">
+          <img
+            class="aside--item-img"
+            :src="require('~/static/img/' + item.image)"
+            :alt="item.title"
+          />
+          <figcaption class="aside--item-caption">
+            <p>{{ item.title }}</p>
+          </figcaption>
+        </figure>
+      </li>
+    </ul>
+    <!-- outro -->
+  </aside>
+</template>
+
+<script>
+export default {
+  props: {
+    dataSource: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="postcss">
+.aside--list {
+  @apply flex flex-wrap justify-evenly items-start
+  my-2;
+
+  @screen lg {
+    @apply my-6;
+  }
+}
+.aside--list-item {
+  @apply mt-0 mb-2;
+  flex: 0 0 30%;
+
+  &:last-child {
+    @apply mb-0;
+  }
+
+  @screen lg {
+    @apply mx-6;
+    flex: 1 1 auto;
+  }
+}
+.aside--item-fig {
+}
+.aside--item-img {
+  @apply relative mx-auto w-full h-auto;
+}
+.aside--item-caption {
+  @apply text-left pt-2 text-xs tracking-tight;
+}
+</style>
--- pages/biography/index.vue
+++ pages/biography/index.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="container">
-    <h1 class="page-title">Biography</h1>
-    <nuxt-content :document="page" />
+    <div class="inner-container">
+      <h1 class="page-title">Biography</h1>
+      <nuxt-content :document="page" />
+    </div>
+    <PageAside class="aside-container" :dataSource="asideContent"></PageAside>
   </div>
 </template>
 
 <script>
+import asideContent from "static/data/bio-aside.json";
 export default {
   async asyncData({ $content }) {
     const page = await $content("biography").fetch();
-    console.log(">asyncData", page);
-    return { page };
+    return { page, asideContent };
+  },
+  mounted() {
+    console.log(asideContent);
   }
 };
 </script>
+
+<style lang="postcss" scoped>
+/* 2 Cols layout withthe aside on right hand side */
+.container {
+  @apply flex flex-wrap justify-between items-stretch;
+}
+.inner-container {
+  flex: 1 1 100%;
+  @screen lg {
+    flex: 1 1 0%;
+  }
+}
+.aside-container {
+  @apply ml-0 mt-8 bg-gray-300;
+  max-width: inherit;
+
+  @screen lg {
+    @apply ml-16;
+    flex: 0 0 auto;
+    max-width: 190px;
+  }
+}
+</style>
