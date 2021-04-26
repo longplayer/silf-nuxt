2a05dc9c50875a57360f9ef277bc13e810948f3c
longplayer <bartaber@hotmail.com>
2021-04-26 12:25:51 +0200

    Added new page: monography

---
+++ pages/monography/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1 class="page-title">Monography</h1>
+    <viewer :dataSource="dataSource" :compConfig="{ gridSize: 4 }"></viewer>
+  </div>
+</template>
+
+<script>
+import db from "static/data/monography.json";
+export default {
+  computed: {
+    dataSource() {
+      return db;
+    }
+  }
+};
+</script>
